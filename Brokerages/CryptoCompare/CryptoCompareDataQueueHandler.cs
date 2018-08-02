6431b0ad76f3a21f2a4f4a09275704c4f823937c
AnshulYADAV, Computer Artist (CoArsey) <AnshulYADAV007@users.noreply.github.com>
2018-08-02 18:11:17 +0530

    Initial Commit with Tests and Queue Handler Class

---
+++ Brokerages/CryptoCompare/CryptoCompareDataQueueHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using QuantConnect.Data;
+using QuantConnect.Interfaces;
+using QuantConnect.Packets;
+
+namespace QuantConnect.Brokerages.CryptoCompare
+{
+    /// <summary>
+    /// Crypto compare data queue handler.
+    /// </summary>
+    public class CryptoCompareDataQueueHandler : IDataQueueHandler
+    {
+        /// <summary>
+        /// Initializes a new instance of the
+        /// <see cref="T:QuantConnect.Brokerages.CryptoCompare.CryptoCompareDataQueueHandler"/> class.
+        /// </summary>
+        public CryptoCompareDataQueueHandler()
+        {
+
+        }
+
+        /// <summary>
+        /// Gets the next ticks.
+        /// </summary>
+        /// <returns>The next ticks.</returns>
+        public IEnumerable<BaseData> GetNextTicks()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Subscribe the specified job and symbols.
+        /// </summary>
+        /// <param name="job">Job.</param>
+        /// <param name="symbols">Symbols.</param>
+        public void Subscribe(LiveNodePacket job, IEnumerable<Symbol> symbols)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Unsubscribe the specified job and symbols.
+        /// </summary>
+        /// <param name="job">Job.</param>
+        /// <param name="symbols">Symbols.</param>
+        public void Unsubscribe(LiveNodePacket job, IEnumerable<Symbol> symbols)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tests/Brokerages/CryptoCompare/CryptoCompareDataQueueHandlerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+using QuantConnect.Brokerages.CryptoCompare;
+
+namespace QuantConnect.Tests.Brokerages.CryptoCompare
+{
+    [TestFixture]
+    public class CryptoCompareDataQueueHandlerTests
+    {
+        [Test]
+        public void TestGetNextTick()
+        {
+            try
+            {
+                var queue = new CryptoCompareDataQueueHandler();
+                Assert.IsNotNull(queue);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+
+        }
+    }
+}
